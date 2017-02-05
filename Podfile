92fb894d9b2dc8c2b57aadd0ffef1242e2435329
Alexandru Corut <r3dm4n@me.com>
2017-02-05 15:07:25 +0200

    UI

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'Weather' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.3'
+end
---
+++ Weather/WeatherVC.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherVC.swift
+//  Weather
+//
+//  Created by r3d on 05/02/2017.
+//  Copyright © 2017 r3d. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherType: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+
+}
+
